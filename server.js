aaf68e0b427ac60442555498d973b93010744bdd
Xavi <xavi.rmz@gmail.com>
2010-06-28 19:50:24 -0400

    Initial commit of full stack js site

---
+++ server.js
@@ -0,0 +1,38 @@
+var sys = require("sys");
+var url = require("url");
+var fs = require("fs");
+var bind = require("./libraries/bind-js");
+var markdown = require("./libraries/markdown-js/lib/markdown");
+var srv = require("./libraries/xavlib/simple-router");
+var chn = require("./libraries/xavlib/channel");
+
+var DefaultBindHandler = (function() {
+    function handler(context, req, res) {
+        bind.toFile("./content/templates/default.html", context, function(data) {
+            res.writeHead(200, { "Conent-Length": data.length,
+                                 "Content-Type": "text/html" });
+            res.end(data, "utf8");
+        });
+    }
+    
+    function bindMarkdown(callback, val) {
+        callback(markdown.renderJsonML(markdown.toHTMLTree(val,"Maruku")), {}, true); 
+    }
+
+    return function(path, extContext) {
+        var context = { markdown: bindMarkdown,
+                        content: function(callback) { callback("(: file ~ " + path + " :)"); } };
+        
+        for(var i in extContext) { context[i] = extContext[i]; }
+        
+        return function(req, res) { handler(context, req, res); }; 
+    };
+})();
+
+srv.error = DefaultBindHandler("./content/404.html");
+
+srv.urls["/robots.txt"] = DefaultBindHandler("./content/robots.txt");
+
+srv.urls["/"] = srv.urls["/index.html"] = DefaultBindHandler("./content/index.html", "home");
+
+srv.server.listen(8005);
